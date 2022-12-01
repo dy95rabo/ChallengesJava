d1fce01cbca71f929752bdcc1197781dc6504dae
dy95rabo <ivoheberle@gmail.com>
2022-12-01 14:17:07 +0100

    added PairMAnagement

---
+++ SehrEinfach/PairManagement.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class PairManagement {
+    /**
+     * Pair Management
+     * 
+     * Given two arguments, return an array which contains these two arguments.
+     * Examples
+     * 
+     * makePair(1, 2) ➞ [1, 2]
+     * 
+     * makePair(51, 21) ➞ [51, 21]
+     * 
+     * makePair(512124, 215) ➞ [512124, 215]
+     * 
+     * Notes
+     * 
+     * N/A
+     */
+    public static int[] makePair(int a, int b) {
+        // Write your code here!
+
+        
+    }
+
+    // short little more advanced solution
+    // public static int[] makePair(int ... a) {
+    // return a;
+    // }
+
+    public static void main(String[] args) {
+        int[][] numbers = new int[][] { { 6, 7 }, { 20, 10 }, { 2, 9 },{1,2},{15,35}, {20,40},{51,21},{512124,215},{34345435,678}};
+        int[][] solutions = numbers;
+
+        for (int i = 0; i < solutions.length; i++) {
+            int []tmp = makePair(numbers[i][0], numbers[i][1]);
+            System.out.println("Your solution was: \t\t[ " + tmp[0]+", "+tmp[1]+" ]"  );
+            System.out.println("The expected solution is: \t[ " + solutions[i][0] +", "+solutions[i][1] + " ]\t\t");
+            System.out.print("Test " + (i + 1) + " was: \t\t\t");
+            System.out.println(Arrays.equals(makePair(numbers[i][0], numbers[i][1]),solutions[i]) + "\n");
+        }
+
+    }
+}
