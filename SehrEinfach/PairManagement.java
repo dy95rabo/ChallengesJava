f6f128297c60d445eebaea6810c1a0ca67f437bc
dy95rabo <ivoheberle@gmail.com>
2022-12-01 14:40:10 +0100

    some small changes

--- SehrEinfach/PairManagement.java
+++ SehrEinfach/PairManagement.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class PairManagement {
     /**
@@ -21,7 +20,6 @@ public class PairManagement {
     public static int[] makePair(int a, int b) {
         // Write your code here!
 
-        
     }
 
     // short little more advanced solution
@@ -30,15 +28,16 @@ public class PairManagement {
     // }
 
     public static void main(String[] args) {
-        int[][] numbers = new int[][] { { 6, 7 }, { 20, 10 }, { 2, 9 },{1,2},{15,35}, {20,40},{51,21},{512124,215},{34345435,678}};
+        int[][] numbers = new int[][] { { 6, 7 }, { 20, 10 }, { 2, 9 }, { 1, 2 }, { 15, 35 }, { 20, 40 }, { 51, 21 },
+                { 512124, 215 }, { 34345435, 678 } };
         int[][] solutions = numbers;
 
         for (int i = 0; i < solutions.length; i++) {
-            int []tmp = makePair(numbers[i][0], numbers[i][1]);
-            System.out.println("Your solution was: \t\t[ " + tmp[0]+", "+tmp[1]+" ]"  );
-            System.out.println("The expected solution is: \t[ " + solutions[i][0] +", "+solutions[i][1] + " ]\t\t");
+            int[] tmp = makePair(numbers[i][0], numbers[i][1]);
+            System.out.println("Your solution was: \t\t[ " + tmp[0] + ", " + tmp[1] + " ]");
+            System.out.println("The expected solution is: \t[ " + solutions[i][0] + ", " + solutions[i][1] + " ]\t\t");
             System.out.print("Test " + (i + 1) + " was: \t\t\t");
-            System.out.println(Arrays.equals(makePair(numbers[i][0], numbers[i][1]),solutions[i]) + "\n");
+            System.out.println(Arrays.equals(makePair(numbers[i][0], numbers[i][1]), solutions[i]) + "\n");
         }
 
     }
