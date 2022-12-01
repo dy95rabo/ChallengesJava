2bb84af5f3ac46d86fe03f9ab093295cc1b52f53
dy95rabo <ivoheberle@gmail.com>
2022-12-01 12:34:30 +0100

    added SevenBoom

---
+++ Schwer/SevenBoom.java
@@ -0,0 +1,65 @@
+package Schwer;
+
+public class SevenBoom {
+    /**
+     * Seven Boom!
+     * 
+     * Create a function that takes an array of numbers and return "Boom!" if the
+     * digit 7 appears in the array. Otherwise, return "there is no 7 in the array".
+     * Examples
+     * 
+     * sevenBoom([1, 2, 3, 4, 5, 6, 7]) ➞ "Boom!"
+     * 7 contains the number seven.
+     * 
+     * sevenBoom([8, 6, 33, 100]) ➞ "there is no 7 in the array"
+     * None of the items contain 7 within them.
+     * 
+     * sevenBoom([2, 55, 60, 97, 86]) ➞ "Boom!"
+     * 97 contains the number seven.
+     * 
+     * Notes
+     * 
+     * N/A
+     */
+    public static String sevenBoom(int[] arr) {
+        for (int i : arr) {
+            if(contains7(i)){
+                return "Boom!";
+            }
+        }
+        return "there is no 7 in the array";
+
+    }
+
+    private static boolean contains7(int i) {
+        while(i!=0){
+            if(i%10 == 7){
+                return true;
+            }
+            i/=10;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] numbers = new int[][] {
+                { 2, 6, 7, 9, 3 },
+                { 33, 68, 400, 5 },
+                { 86, 48, 100, 66 },
+                { 76, 55, 44, 32 },
+                { 35, 4, 9, 37 }
+        };
+        boolean[] solutions = new boolean[] { true, false, false, true, true };
+
+        for (int i = 0; i < solutions.length; i++) {
+            System.out.println("Your solution was: \t\t" + sevenBoom(numbers[i]));
+            System.out.println("The expected solution is: \t" + (solutions[i] ? "Boom!" : "there is no 7 in the array") + "\t\t");
+            System.out.print("Test " + (i + 1) + " was: \t\t\t");
+            System.out.println(
+                    sevenBoom(numbers[i]).equals(solutions[i] ? "Boom!" : "there is no 7 in the array") + "\n");
+        }
+
+    }
+
+}
+
